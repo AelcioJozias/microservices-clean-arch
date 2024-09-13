856cd27b8090b7800db48b7c8a8cfaa8db21e97f
jozias <aelciojoziasp@gmaio.com>
2024-09-13 07:02:52 -0300

    estrutura inicial do microserviço de patient

---
+++ services/patient/partient/src/main/java/com/br/jozias/services/partient/StartPatient.java
@@ -0,0 +1,13 @@
+package com.br.jozias.services.partient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartPatient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartPatient.class, args);
+	}
+
+}
---
+++ services/patient/partient/src/test/java/com/br/jozias/services/partient/StartPatientTests.java
@@ -0,0 +1,13 @@
+package com.br.jozias.services.partient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartPatientTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
